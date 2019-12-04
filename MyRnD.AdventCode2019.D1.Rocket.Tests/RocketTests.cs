a84fddc58385e91629b9876edc1c8e51ceb1914a
Joao Morais <jmorais@iprotech.com>
2019-12-03 16:16:55 -0800

    Day 01 - Rocket Fuel calculation

---
+++ MyRnD.AdventCode2019.D1.Rocket.Tests/RocketTests.cs
@@ -0,0 +1,150 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyRnD.AdventCode2019.D1.Rocket.Tests
+{
+    [TestClass]
+    public class RocketTests
+    {
+        private Rocket _rocket;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _rocket = new Rocket();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _rocket = null;
+        }
+
+        #region Part A
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartAExample1()
+        {
+            _rocket.Modules.Add(new Module {Mass = 12});
+
+            Assert.AreEqual(1, _rocket.Modules.Count);
+            Assert.AreEqual(2, _rocket.FuelCounterUpper);
+        }
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartAExample2()
+        {
+            _rocket.Modules.Add(new Module { Mass = 14 });
+
+            Assert.AreEqual(1, _rocket.Modules.Count);
+            Assert.AreEqual(2, _rocket.FuelCounterUpper);
+        }
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartAExample3()
+        {
+            _rocket.Modules.Add(new Module { Mass = 1969 });
+
+            Assert.AreEqual(1, _rocket.Modules.Count);
+            Assert.AreEqual(654, _rocket.FuelCounterUpper);
+        }
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartAExample4()
+        {
+            _rocket.Modules.Add(new Module { Mass = 100756 });
+
+            Assert.AreEqual(1, _rocket.Modules.Count);
+            Assert.AreEqual(33583, _rocket.FuelCounterUpper);
+        }
+
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartAExampleFrom1To4()
+        {
+            _rocket.Modules.Add(new Module { Mass = 12 });
+            _rocket.Modules.Add(new Module { Mass = 14 });
+            _rocket.Modules.Add(new Module { Mass = 1969 });
+            _rocket.Modules.Add(new Module { Mass = 100756 });
+
+            Assert.AreEqual(4, _rocket.Modules.Count);
+            Assert.AreEqual(2 + 2 + 654 + 33583, _rocket.FuelCounterUpper);
+        }
+
+        #endregion
+
+        #region Part B - Fuel itself requires fuel just like a module
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartBExample1()
+        {
+            _rocket.Modules.Add(new Module { Mass = 12 });
+
+            Assert.AreEqual(1, _rocket.Modules.Count);
+            Assert.AreEqual(2, _rocket.FuelCounterUpper2);
+        }
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartBExample2()
+        {
+            _rocket.Modules.Add(new Module { Mass = 14 });
+
+            Assert.AreEqual(1, _rocket.Modules.Count);
+            Assert.AreEqual(2, _rocket.FuelCounterUpper2);
+        }
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartBExample3()
+        {
+            _rocket.Modules.Add(new Module { Mass = 1969 });
+
+            Assert.AreEqual(1, _rocket.Modules.Count);
+            Assert.AreEqual(654 + 216 + 70 + 21 + 5, _rocket.FuelCounterUpper2);
+        }
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartBExample4()
+        {
+            _rocket.Modules.Add(new Module { Mass = 100756 });
+
+            Assert.AreEqual(1, _rocket.Modules.Count);
+            Assert.AreEqual(33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2, _rocket.FuelCounterUpper2);
+        }
+
+
+        [TestMethod]
+        public void Rocket_FuelCounterUpper_PartBExampleFrom1To4()
+        {
+            _rocket.Modules.Add(new Module { Mass = 12 });
+            _rocket.Modules.Add(new Module { Mass = 14 });
+            _rocket.Modules.Add(new Module { Mass = 1969 });
+            _rocket.Modules.Add(new Module { Mass = 100756 });
+
+            Assert.AreEqual(4, _rocket.Modules.Count);
+            Assert.AreEqual(2 + 2 + (654 + 216 + 70 + 21 + 5) + (33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2), _rocket.FuelCounterUpper2);
+        }
+
+        #endregion
+
+
+        [TestMethod]
+        public void RocketBool()
+        {
+            string[] values = { null, String.Empty, "True", "False",
+                "true", "false", "    true    ", "0",
+                "1", "-1", "string" };
+            foreach (var value in values)
+            {
+                bool flag;
+                if (Boolean.TryParse(value, out flag))
+                    Console.WriteLine("'{0}' --> {1}", value, flag);
+                else
+                    Console.WriteLine("Unable to parse '{0}' flag: '{1}'.",
+                        value == null ? "<null>" : value, flag);
+            }
+
+            bool b = bool.Parse("1");
+            Console.WriteLine($"bool.Parse(\"1\") => '{b}'");
+        }
+    }
+}
---
+++ MyRnD.AdventCode2019.D1.Rocket/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MyRnD.AdventCode2019.D1.Rocket
+{
+
+    public sealed class Program
+    {
+        const string DefaultFilename = @"Rocket-day1.txt";
+
+        static void Main(string[] args)
+        {
+            string fullFilename = Path.Combine(Environment.CurrentDirectory, DefaultFilename);
+            Console.WriteLine($"Loading rocket data from '{fullFilename}'...");
+
+            var rocketDay1 = Factory.GetRocketFromFile(fullFilename);
+            int fuelNeeded = rocketDay1.FuelCounterUpper;
+            int fuelNeeded2 = rocketDay1.FuelCounterUpper2;
+            int extraFuel = fuelNeeded2 - fuelNeeded;
+            Console.WriteLine($"The rocket has #{fullFilename} module(s) and the sum of the fuel requirements is '{fuelNeeded}'.");
+            Console.WriteLine($"However, after taking into account the 'fuel needed for the fuel' the total sum of the fuel requirements is '{fuelNeeded2}' (an extra '{extraFuel}').");
+        }
+    }
+
+    public sealed class Module
+    {
+        public int Mass { get; set; }
+
+        /// <summary>
+        /// Simple calculation just for the module's mass.
+        /// </summary>
+        public int FuelCounterUpper
+        {
+            get { return ((int) Math.Floor(Mass / 3.0) - 2); }
+        }
+
+        /// <summary>
+        /// Calculates the fuel required by the module and for the fuel itself.
+        /// </summary>
+        public int FuelCounterUpper2
+        {
+            get
+            {
+                int totalFuelNeeded = 0;
+                int mass = Mass;
+                while (mass > 0)
+                {
+                    int fuelNeeded = ((int) Math.Floor(mass / 3.0) - 2);
+                    if (fuelNeeded > 0)
+                        totalFuelNeeded += fuelNeeded;
+                    mass = fuelNeeded;
+                }
+
+                return totalFuelNeeded;
+            }
+        }
+
+    }
+
+    public sealed class Rocket
+    {
+        public Rocket()
+            : this(new List<Module>())
+        {
+        }
+
+        public Rocket(List<Module> modules)
+        {
+            Modules = modules;
+        }
+
+        public List<Module> Modules { get; set; }
+
+        public int FuelCounterUpper
+        {
+            get
+            {
+                int total = 0;
+                if (Modules != null && Modules.Any())
+                {
+                    foreach (var module in Modules)
+                    {
+                        total += module.FuelCounterUpper;
+                    }
+                }
+                return total;
+            }
+        }
+
+        public int FuelCounterUpper2
+        {
+            get
+            {
+                int total = 0;
+                if (Modules != null && Modules.Any())
+                {
+                    foreach (var module in Modules)
+                    {
+                        total += module.FuelCounterUpper2;
+                    }
+                }
+                return total;
+            }
+        }
+
+    }
+
+    public abstract class Factory
+    {
+        const int BufferSize = 1024;
+
+        public static Rocket GetRocketFromFile(string fullFileName)
+        {
+            var tempRocket = new Rocket();
+            using (var fileStream = File.OpenRead(fullFileName))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    // line has only one value
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        int moduleMass = int.Parse(line);
+                        var tempModule = new Module {Mass = moduleMass};
+                        tempRocket.Modules.Add(tempModule);
+                    }
+                }
+            }
+            return tempRocket;
+        }
+    }
+}
